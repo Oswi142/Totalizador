8a7ffc8fc1db3fe38a0d784ff11d98b7c5a23a24
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-02-22 23:34:16 -0400

    Mostrar cantidad items

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,9 @@
-import sumar from "./sumador";
-
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const cant = document.querySelector("#cant");
+const form = document.querySelector("#ventas-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + cant.value + "</p>";
 });
